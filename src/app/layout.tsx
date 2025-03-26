b92c2d76793a608ab2f213cdd76e05998db8a9af
Rafael Odassi <rafael.odassi@lecom.com.br>
2025-03-25 21:17:33 -0300

    Initial commit.

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
+import StyledComponentsRegistry from '../lib/registry';
+
+import './globals.css';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Bloom Books',
+  description: 'Bloom Books',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={roboto.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from './page.module.css';
+
+const Home = () => <div className={styles.page}>teste</div>;
+
+export default Home;
